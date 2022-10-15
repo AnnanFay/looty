434747629aed876e52a06e503b5a582eac8390d4
Annan Fay Yearian <annanfay@gmail.com>
2022-10-15 17:46:41 +0200

    Fix: Add new format for life/mana gain per kill

--- looty/src/main/scala/looty/model/parsers/AffixesParser.scala
+++ looty/src/main/scala/looty/model/parsers/AffixesParser.scala
@@ -206,16 +206,22 @@ object AffixesParser {
   }
 
   for (x <- LifeAndMana.all) {
-    regex1(s"^Regenerate ([.\\d]+) ${x.cap} per second$")(_.regenPerSecond.flat.+=(x, _))
-    regex1(s"^Regenerate ([.\\d]+)% of ${x.cap} per second$")(_.regenPerSecond.percent(x) += _)
+    regex1(s"^Regenerate ([.\\d]+) ${x.cap} per second$$")(_.regenPerSecond.flat.+=(x, _))
+    regex1(s"^Regenerate ([.\\d]+)% of ${x.cap} per second$$")(_.regenPerSecond.percent(x) += _)
     // $ prevents 'while Frozen' and similar for being parsed
-    
+
     regex1(s"^([+-\\d]+) ${x.cap} [gG]ained on Kill")(_.onKill.lifeAndMana(x) += _)
     regex1(s"^Recover ([.\\d]+)% of ${x.cap} on Kill")(_.recoverOnKill.lifeAndMana(x) += _)
+    // alternative format
+    regex1(s"Gain ([+-\\d]+) ${x.cap} per Enemy Killed")(_.onKill.lifeAndMana.+=(x, _))
+
     regex1(s"^([+-\\.\\d]+)% of Physical Attack Damage Leeched as ${x.cap}")(_.leech.physical(x) += _)
     plusTo(s"maximum ${x.cap}")(_.plusTo.lifeAndMana.+=(x, _))
+
     simple1("", s"${x.cap} gained for each Enemy hit by Attacks")(_.onAttackHit.lifeAndMana.+=(x, _))
     simple1("", s"${x.cap} gained for each Enemy hit by your Attacks")(_.onAttackHit.lifeAndMana.+=(x, _))
+    // alternative format
+    regex1(s"Gain ([+-\\d]+) ${x.cap} per Enemy Hit with Attacks")(_.onKill.lifeAndMana.+=(x, _))
   }
 
   regex1(s"^Recover ([.\\d]+)% of Energy Shield on Kill")(_.recoverOnKill.energyShield += _)
